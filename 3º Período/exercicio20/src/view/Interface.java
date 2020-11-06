bde2c99ec00d8103fb196135dd4c262f96ff4872
Gabriel Galdino <ggaldino95@gmail.com>
2020-11-06 11:54:04 -0300

    Exercício 20 sobre Introdução a Leitura de Arquivos

---
+++ 3º Período/exercicio20/src/view/Interface.java
@@ -0,0 +1,28 @@
+package view;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Interface {
+
+	public static void main(String[] args) {
+
+		File arquivo = new File("C:\\Users\\GGald\\Desktop\\FACULDADE\\Workspaces\\JAVA\\Arquivo\\in.txt");
+		Scanner sc = null;
+
+		// Instanciar o Scanner a partir do arquivo
+		try { // Deve-se usar o bloco 'try' pois ao tentar abrir o arquivo, pode ser gerada uma exceção do tipo IOException
+			sc = new Scanner(arquivo);
+			while (sc.hasNextLine()) { // hasNextLine(): Testa se ainda existe uma nova linha no arquivo
+				System.out.println(sc.nextLine()); // Impressão da linha no arquivo
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally { // bloco para fechamento do recurso independente se der certo i 'try' ou não
+			if (sc != null) 
+				sc.close();
+		}
+	}
+}
